895fa9d7d539fd1b04e3ec43b630924e64254124
Pavlo <pmishchenko-ua@memsql.com>
2023-02-06 17:06:54 +0200

    Bump version to 1.5.3

--- client_config.h
+++ client_config.h
@@ -2,7 +2,7 @@
 #define CLIENT_CONFIG_H
 
 #define S2_SERVER_VERSION "7.8.10"
-#define S2_CLIENT_VERSION "1.5.2"
+#define S2_CLIENT_VERSION "1.5.3"
 #define MARIADB_VERSION_USED "3.2.5"
 
 #endif  // CLIENT_CONFIG_H
